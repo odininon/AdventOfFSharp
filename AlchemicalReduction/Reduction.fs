ad4af3640b0cc0dc7891fe3181be28f60cb12fd9
Michael Stengel <mike.stengel@gmail.com>
2018-12-29 12:26:39 -0600

    Compose only once

--- AlchemicalReduction/Reduction.fs
+++ AlchemicalReduction/Reduction.fs
@@ -6,10 +6,10 @@ module Reduction =
     let removeDuplicate (a:char) (b: string) =
         b.Replace(a.ToString() + char(int a - 32).ToString(), "").Replace(char(int a - 32).ToString() + a.ToString() , "")
 
-    let fs = [97..122] |> List.map(fun i -> (removeDuplicate (char i)))
+    let fs = [97..122] |> List.map(fun i -> (removeDuplicate (char i))) |> compose
     let removeDuplicates a = 
         let rec _do _a cycles =
-          let reduced = (compose fs _a)
+          let reduced = fs _a
           if _a.Equals reduced then 
             reduced else (_do reduced (cycles + 1))
         _do a 1
